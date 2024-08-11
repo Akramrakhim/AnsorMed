e7eaaf70a44a45c5346d7cc94d3a6acd8d3942e9
akramrakhim <akramrakhimov4@gmail.com>
2024-08-11 21:56:38 +0300

    make site header responsive

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const elSiteHeader = document.querySelector('.site-header');
+const elSiteHeaderToggleButton = elSiteHeader.querySelector('.site-header-toggle-button');
+
+if (elSiteHeaderToggleButton) {
+    elSiteHeaderToggleButton.addEventListener('click', function() {
+        elSiteHeader.classList.toggle('site-header-open');
+        
+    }); 
+}
